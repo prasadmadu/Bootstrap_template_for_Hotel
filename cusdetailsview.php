c9ad8ac50530809eb0744f7ebf27dd85cb43f2f4
prasadmadu <37903737+prasadmadu@users.noreply.github.com>
2020-05-26 22:21:12 +0530

    Add files via upload

---
+++ cusdetailsview.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<style>
+    #head{
+        background-image: url("images/home.jpg");
+
+    }
+    #foot{
+        background-image: url("images/footer.jpg");
+    }
+    .carousel-inner img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
+</head>
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<body>
+
+
+<div class="" id="head">
+    <div class="row" id="hr1">
+        <div class="col-sm-12" id="headerlogo">
+            <br>
+            <img src="images/logo_4.png" alt="" id="logoimage">
+            <br>
+            <br>
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row sticky-top" id="hr2">
+        <div class="col-sm-3" id="">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                <a class="nav-link" href="https://www.facebook.com/"><i class="fa fa-facebook-square" style="font-size:24px"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/"><i class="fa fa-linkedin-square" style="font-size:24px"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="https://twitter.com/"><i class="fa fa-twitter-square" style="font-size:24px"></i></a></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="https://www.youtube.com"><i class="fa fa-youtube-square" style="font-size:24px"></i></a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-sm-6" id="">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Home</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>About Us</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Rooms</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Tours</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Contact Us</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Staff</b></a>
+                </li>
+            </ul>
+
+        </div>
+
+        <div class="col-sm-3" id="">
+            <div  class="row">
+                <div  class="col-sm-3" id=""></div>
+                <div  class="col-sm-9" id="navbutton">
+                    <p id="navbook"><b>Book Your Room Now</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div  class="row">
+        <div  class="col-sm-12" id="headerline"></div>
+    </div>
+    
+<!--*******************************************************************-->
+    <div class="row" id="hr3">
+        <div class="col-sm-12" id="">
+            <p>
+                <span id="headertoptext">
+                    <i><b>Book Your Stay</b></i>
+                </span>
+                <br>
+                <span id="headermidtext">
+                    See what a difference a stay makes
+                </span>
+            </p>
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="hr4">
+        <div class="col-sm-5" id=""></div>
+        <div class="col-sm-2" id="headerbook">
+            
+            <p>Book Now</p>
+        </div>
+        <div class="col-sm-5" id=""></div>
+    </div>
+<!--*******************************************************************-->
+    <form action="">
+        <div class="form-group">
+            <div class="row" id="hr5">
+                <div class="col-sm-1" id="">
+                    
+                </div>
+                
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Check In:</label>
+                            <input type="date" class="form-control" id="" name="" placeholder="">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Check Out:</label>
+                            <input type="date" class="form-control" id="" name="" placeholder="Chech Out">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Adults:</label>
+                            <input type="number" class="form-control" id="" name="" placeholder="Adults">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Children:</label>
+                            <input type="number" class="form-control" id="" name="" placeholder="Children">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">&nbsp</label>
+                            <button type="submit" class="form-control submitbutton">Check Available</button>
+                        </div>
+                    
+                <div class="col-sm-1" id="">
+                    
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+
+<div class="" id="body">
+<!--*******************************************************************--> 
+    <div class="row" id="cusbr1">
+        <div class="col-sm-1" id="">
+                    
+        </div>
+        <div class="col-sm-10" id="">
+        <br><br>
+            <h2><i>customer name</i></h2>
+        </div>
+        <div class="col-sm-1" id="">
+                    
+        </div>            
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="cusbr2">
+        <div class="col-sm-1" id="">
+                    
+        </div>
+        <div class="col-sm-5" id="">
+        <br><br>
+            <p><b><i>Date &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp:</i></b>
+            <i>2019-02-20</i>
+            </p>
+            <br>
+            <p><b><i>Estimated Arrival Time :</i></b>
+            <i>10.30 pm</i>
+            </p>
+            <br>
+            <p><b><i>Adults  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp:</i></b>
+            <i>2</i>
+            </p>
+            <br>
+            <p><b><i>Children &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp:</i></b>
+            <i>3</i>
+            </p>
+            <br>
+            <p><b><i>Address  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp:</i></b>
+            <i>line1,  
+            line2, 
+            line3
+            </i>
+            </p>
+            <br>
+            <p><b><i>E-mail  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp:</i></b>
+            <i>ncprasadmadusanka@gmail.com</i>
+            </p>
+            <br>
+            <p><b><i>Telephone No  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp:</i></b>
+            <i>077-6657638</i>
+            </p>
+            <br><br>
+        </div>
+        <div class="col-sm-5" id="">
+        <br><br>
+            <p><b><i>Room Type :</i></b></p>
+            
+            <img src="images/room_4.jpg" alt="" width="100%">
+            
+            <p><b><i>Price :</i></b></p>
+            <br> 
+        </div>
+        <div class="col-sm-1" id="">
+            
+        </div>            
+    </div>   
+<!--*******************************************************************-->
+    
+<!--*******************************************************************-->
+    
+<!--*******************************************************************-->
+    
+<!--*******************************************************************-->
+    
+<!--*******************************************************************-->
+    
+<!--*******************************************************************-->
+</div>
+
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+
+<div class="" id="foot">
+    <div  class="row">
+        <div  class="col-sm-12" id="headerline"></div>
+    </div>
+    <div class="row" id="fr1">
+        <div class="col-sm-12" id="footerlogo">
+            <br>
+            <img src="images/logo_4.png" alt="" id="logoimage">
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+    <div class="row" id="fr2">
+        <div class="col-sm-1" id=""></div>
+        <div class="col-sm-3" id="">
+            <p>
+                <b><i>Phone:</i></b>
+                <br>
+                <b>077-6657638</b>
+            </p>
+        </div>
+        <div class="col-sm-4" id="">
+            <p>
+                <b><i>Address:</i></b>
+                <br>
+                <b><i>
+                    Blue sphere Rock Hotel,
+                    <br>
+                    Lizzie villa Rd,
+                    <br>
+                    Ella.
+                </i></b>
+                <br>
+                
+            </p>
+        </div>
+        <div class="col-sm-3" id="">
+            <p>
+                <b><i>E-mail:</i></b>
+                <br>
+                <b><i>ncprasadmadusanka@gmail.com</i></b>
+            </p>
+        </div>
+        <div class="col-sm-1" id=""></div>
+        
+    </div>
+    <div  class="row">
+        <div  class="col-sm-12" id="headerline"></div>
+    </div>
+    <div  class="row">
+        <div  class="col-sm-12" id="fr3">
+            <br>
+            <p>Copyright &copy; 2018 Blue sphere Rock Hotel</p>
+            <br>
+        </div>
+    </div>
+
+    
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<style>
+    #head{
+        background-image: url("images/home.jpg");
+
+    }
+    #foot{
+        background-image: url("images/footer.jpg");
+    }
+    .carousel-inner img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
+</head>
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<body>
+
+
+<div class="" id="head">
+    <div class="row" id="hr1">
+        <div class="col-sm-12" id="headerlogo">
+            <br>
+            <img src="images/logo_4.png" alt="" id="logoimage">
+            <br>
+            <br>
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row sticky-top" id="hr2">
+        <div class="col-sm-3" id="">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                <a class="nav-link" href="https://www.facebook.com/"><i class="fa fa-facebook-square" style="font-size:24px"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/"><i class="fa fa-linkedin-square" style="font-size:24px"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="https://twitter.com/"><i class="fa fa-twitter-square" style="font-size:24px"></i></a></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="https://www.youtube.com"><i class="fa fa-youtube-square" style="font-size:24px"></i></a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-sm-6" id="">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Home</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>About Us</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Rooms</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Tours</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Contact Us</b></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#"><b>Staff</b></a>
+                </li>
+            </ul>
+
+        </div>
+
+        <div class="col-sm-3" id="">
+            <div  class="row">
+                <div  class="col-sm-3" id=""></div>
+                <div  class="col-sm-9" id="navbutton">
+                    <p id="navbook"><b>Book Your Room Now</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div  class="row">
+        <div  class="col-sm-12" id="headerline"></div>
+    </div>
+    
+<!--*******************************************************************-->
+    <div class="row" id="hr3">
+        <div class="col-sm-12" id="">
+            <p>
+                <span id="headertoptext">
+                    <i><b>Book Your Stay</b></i>
+                </span>
+                <br>
+                <span id="headermidtext">
+                    See what a difference a stay makes
+                </span>
+            </p>
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="hr4">
+        <div class="col-sm-5" id=""></div>
+        <div class="col-sm-2" id="headerbook">
+            
+            <p>Book Now</p>
+        </div>
+        <div class="col-sm-5" id=""></div>
+    </div>
+<!--*******************************************************************-->
+    <form action="">
+        <div class="form-group">
+            <div class="row" id="hr5">
+                <div class="col-sm-1" id="">
+                    
+                </div>
+                
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Check In:</label>
+                            <input type="date" class="form-control" id="" name="" placeholder="">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Check Out:</label>
+                            <input type="date" class="form-control" id="" name="" placeholder="Chech Out">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Adults:</label>
+                            <input type="number" class="form-control" id="" name="" placeholder="Adults">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">Children:</label>
+                            <input type="number" class="form-control" id="" name="" placeholder="Children">
+                        </div>
+                        <div class="col-sm-2" id="bookingform">
+                            <label for="">&nbsp</label>
+                            <button type="submit" class="form-control submitbutton">Check Available</button>
+                        </div>
+                    
+                <div class="col-sm-1" id="">
+                    
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+
+<div class="" id="body">
+<!--*******************************************************************--> 
+    <div class="row" id="br1">
+        <div class="col-sm-1" id=""></div>
+        <div class="col-sm-10" id="">
+            <div id="row1background"></div>
+            <div id="row1">
+                <br>
+                <br>
+                <h1 id="br1h1">Amazing Hotel in front of the Mountan</h1>
+                <br>
+                <p>description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description </p>
+                <br>
+            </div>
+        </div>
+        <div class="col-sm-1" id=""></div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="br2">
+        <div class="col-sm-12" id=""></div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="br3">
+        <div class="col-sm-7" id="indexsldeback">
+            <div id="demo" class="carousel slide" data-ride="carousel">
+                <ul class="carousel-indicators">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                    <li data-target="#demo" data-slide-to="2"></li>
+                </ul>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/rooms_1.jpg">   
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/rooms_1.jpg">   
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/rooms_1.jpg">   
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="col-sm-5" id="indexsldeback">
+
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="br4">   
+        <div class="col-sm-5" id="indexsldeback">
+
+        </div>
+        <div class="col-sm-7" id="indexsldeback">
+            <div id="demo" class="carousel slide" data-ride="carousel">
+                <ul class="carousel-indicators">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                    <li data-target="#demo" data-slide-to="2"></li>
+                </ul>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/rooms_2.jpg">   
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/rooms_2.jpg">   
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/rooms_2.jpg">   
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="br5">
+        <div class="col-sm-4" id="">
+            <br>
+            <br>
+            <i class="fa fa-smile-o" id="br5i"></i></i>
+            <br>
+            <br>
+            <h3>Good Hospitality</h3>
+            <br>
+            <p>description description description description description description description description description description description </p>
+        </div>
+        <div class="col-sm-4" id="">
+            <br>
+            <br>
+            <i class="fa fa-shield" id="br5i"></i></i>
+            <br>
+            <br>
+            <h3>Safty</h3>
+            <br>
+            <p>description description description description description description description description description description description </p>
+        </div>
+        <div class="col-sm-4" id="">
+            <br>
+            <br>
+            <i class="fa fa-wifi" id="br5i"></i></i>
+            <br>
+            <br>
+            <h3>Free wifi</h3>
+            <br>
+            <p>description description description description description description description description description description description </p>
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="br6">
+        <div class="col-sm-4" id="br6r">
+            <img src="images/offer_2.jpg" alt="" id="br6image">
+            <h3>Indoor Pool</h3>
+            <p>
+                description description description description description description description description description description description description 
+                <span id="br6items">FREE</span>
+            </p>
+        </div>
+        <div class="col-sm-4" id="br6r">
+            <img src="images/offer_4.jpg" alt="" id="br6image">
+            <h3>Spa Zone</h3>
+            <p>
+                description description description description description description description description description description description description 
+                <span id="br6items">20USD</span>
+            </p>
+        </div>
+        <div class="col-sm-4" id="br6r">
+            <img src="images/offer_5.jpg" alt="" id="br6image">
+            <h3>Indoor Pool</h3>
+            <p>
+                description description description description description description description description description description description description 
+                <span id="br6items">FREE</span>
+            </p>
+        </div>
+    </div>
+<!--*******************************************************************-->
+    <div class="row" id="br7">
+        <div class="col-sm-12" id=""></div>
+    </div>
+<!--*******************************************************************-->
+</div>
+
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+<!--*******************************************************************-->
+
+<div class="" id="foot">
+    <div  class="row">
+        <div  class="col-sm-12" id="headerline"></div>
+    </div>
+    <div class="row" id="fr1">
+        <div class="col-sm-12" id="footerlogo">
+            <br>
+            <img src="images/logo_4.png" alt="" id="logoimage">
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+    <div class="row" id="fr2">
+        <div class="col-sm-1" id=""></div>
+        <div class="col-sm-3" id="">
+            <p>
+                <b><i>Phone:</i></b>
+                <br>
+                <b>077-6657638</b>
+            </p>
+        </div>
+        <div class="col-sm-4" id="">
+            <p>
+                <b><i>Address:</i></b>
+                <br>
+                <b><i>
+                    Blue sphere Rock Hotel,
+                    <br>
+                    Lizzie villa Rd,
+                    <br>
+                    Ella.
+                </i></b>
+                <br>
+                
+            </p>
+        </div>
+        <div class="col-sm-3" id="">
+            <p>
+                <b><i>E-mail:</i></b>
+                <br>
+                <b><i>ncprasadmadusanka@gmail.com</i></b>
+            </p>
+        </div>
+        <div class="col-sm-1" id=""></div>
+        
+    </div>
+    <div  class="row">
+        <div  class="col-sm-12" id="headerline"></div>
+    </div>
+    <div  class="row">
+        <div  class="col-sm-12" id="fr3">
+            <br>
+            <p>Copyright &copy; 2018 Blue sphere Rock Hotel</p>
+            <br>
+        </div>
+    </div>
+
+    
+</div>
+</body>
+</html>
